9b87144e1d078ade247e7ade697661b46b9b3a50
Popescu Daniel <same.dan@gmail.com>
2025-02-01 10:01:40 +0100

    ShadCn UI

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ### Source
 
 > https://www.udemy.com/course/nextjs-ecommerce-course/learn/lecture/47557369#overview
+
+### ShadCN UI
+
+> https://ui.shadcn.com/
+
+> npx shadcn@latest add button
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePAge = () => {
-  return <p>HomePAge</p>;
+  return (
+    <p>
+      HomePAge
+      <Button></Button>
+    </p>
+  );
 };
 
 export default HomePAge;
